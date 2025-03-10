7593d7c994f2958a683953435838ebbbe0df56ba
MEGA <MEGA@gmail.com>
2025-03-10 12:27:22 +0200

    not completed

--- NumberUtils.cs
+++ NumberUtils.cs
@@ -1,9 +1,26 @@
 ﻿using System;
-
-public class NumberUtils
+namespace Number_Tricks
 {
-	public NumberUtils()
-	{
+    public class NumberUtils
+    {
 
-	}
+        public NumberUtils()
+        {
+
+        }
+
+        public static double FastPow(double a, int n)
+        {
+            if (n == 0)
+            {
+                return 1;
+            }
+            double halfPowered = FastPow(a, n / 2);
+            double subTotal = halfPowered * halfPowered;
+            return n % 2 == 0 ? subTotal : subTotal * a;
+        }
+
+
+    }
+
 }
--- Program.cs
+++ Program.cs
@@ -5,6 +5,7 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            NumberUtils.FastPow(2, 11);
             Console.ReadKey();
         }
     }
