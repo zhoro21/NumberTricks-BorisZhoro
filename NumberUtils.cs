726e47023373c686bbf30c5a7315101113b7c116
MEGA <MEGA@gmail.com>
2025-03-14 11:20:09 +0200

    solve problem

--- NumberUtils.cs
+++ NumberUtils.cs
@@ -6,4 +6,17 @@ public class NumberUtils
 	{
 
 	}
+	public static int BabylonRoot(int number)
+	{
+		int a = number;
+		int b = 1;
+		double e =0.000001;
+
+       while (a - b > e)
+        {
+            a = (a + b) / 2;
+            b = number / a;
+        }
+	   return a;
+	}
 }
