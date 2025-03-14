2fe396908ae7f87c51a5d102bb5b4dd399fa7617
MEGA <MEGA@gmail.com>
2025-03-14 12:24:56 +0200

    SieveOfEratosthenes method

--- NumberUtils.cs
+++ NumberUtils.cs
@@ -22,7 +22,7 @@ namespace Number_Tricks
         }
         public static double FastPow(double a, int n)
         {
-            {
+            
                 if (n == 0)
                 {
                     return 1;
@@ -30,8 +30,32 @@ namespace Number_Tricks
                 double halfPowered = FastPow(a, n / 2);
                 double subTotal = halfPowered * halfPowered;
                 return n % 2 == 0 ? subTotal : subTotal * a;
+            
+        }
+
+        public static void SieveOfEratosthenes(int n)
+        {
+            bool[] prime = new bool[n + 1];
+            for (int i = 0; i <= n; i++)
+            {
+                prime[i] = true;
+            }
+            for (int p = 2; p * p <= n; p++)
+            {
+                if (prime[p] == true)
+                {
+                    for (int i = p * p; i <= n; i += p)
+                        prime[i] = false;
+                }
+            }
+            for (int i = 2; i <= n; i++)
+            {
+                if (prime[i] == true)
+                    Console.Write(i + " ");
             }
         }
+
+
     }
 
 }
--- Program.cs
+++ Program.cs
@@ -4,9 +4,13 @@ namespace Number_Tricks
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-            Console.WriteLine("{0:f4}", NumberUtils.BabylonRoot(12565));
-            Console.WriteLine(NumberUtils.FastPow(2, 11));
+            //Console.WriteLine("Hello, World!");
+            //Console.WriteLine("{0:f4}", NumberUtils.BabylonRoot(12565));
+            //Console.WriteLine(NumberUtils.FastPow(2, 11));
+
+            Console.WriteLine("SieveOfEratosthenes:");
+            int n = int.Parse(Console.ReadLine());
+            NumberUtils.SieveOfEratosthenes(n);
             Console.ReadKey();
         }
     }
